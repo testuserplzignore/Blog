014ddc8be6fac3323914adab57467604ba76430c
Robert Morrissey <rbrtmorrissey86@gmail.com>
2019-04-08 14:22:58 -0400

    post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,35 @@ class PostsController < ApplicationController
     render json: @posts
   end
 
-  
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post
+    else render json: { errors: @post.errors }, status: :unprocessable_entity
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      render json: @post, status: :ok
+    else
+      render json: { errors: @post.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    head 204
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :user_id)
+  end
 end
